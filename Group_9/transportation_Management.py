9db4f5ef10f9d805ce09c19723b8d27999b587a6
stephenvillagonzalo <79467722+stephenvillagonzalo@users.noreply.github.com>
2023-03-26 00:22:41 -0400

    Update transportation_Management.py
    Initial Code for Transportation Management

--- Group_9/transportation_Management.py
+++ Group_9/transportation_Management.py
@@ -2,3 +2,28 @@
 Stephen Villagonzalo
 Part 3: Transportation Management
 """
+
+import collections
+
+class Solution:
+    def racecar(self, target: int) -> int:
+        queue = collections.deque([(0, 0, 1)])
+        visited = set()
+
+        while queue:
+            steps, position, speed = queue.popleft()
+
+            if position == target:
+                return steps
+
+            if (position, speed) in visited:
+                continue
+            else:
+                visited.add((position, speed))
+
+                queue.append((steps+1,position+speed,speed*2))
+
+                if (position + speed < target and speed < 0):
+                    queue.append((steps + 1,position, 1))
+                elif (position + speed > target and speed > 0):
+                    queue.append((steps + 1, position, -1))
